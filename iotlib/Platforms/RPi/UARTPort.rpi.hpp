ba782399b536f05743a1d9166715e7e2ebfffa68
Szymon Katra <szymon.katra@gmail.com>
2019-10-27 19:48:59 +0100

    Add UARTPort.rpi.hpp empty file

---
+++ iotlib/Platforms/RPi/UARTPort.rpi.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+//namespace iotlib
+//{
+//
+//
+//    class UARTPort
+//    {
+//    public:
+//        enum class Parity
+//        {
+//            None,
+//            Even,
+//            Odd
+//        };
+//        enum class StopBits
+//        {
+//            One,
+//            OneAndHalf,
+//            Two
+//        };
+//
+//    private:
+//        UARTPortDefinition port;
+//        bool isUART0Swapped;
+//
+//    public:
+//        UARTPort(UARTPortDefinition uartPort, uint32_t baudRate, UARTPort::Parity parity, UARTPort::StopBits stopBits);
+//        ~UARTPort();
+//
+//        size_t write(const uint8_t* buffer, size_t size);
+//        size_t read(uint8_t* buffer, size_t size);
+//
+//        void print(const char* str);
+//
+//    private:
+//        uart_parity_t convertParity(UARTPort::Parity parity);
+//        uart_stop_bits_t convertStopBits(UARTPort::StopBits stopBits);
+//    };
+//}
