0bb22277c0dd2dcc5ed70596208dc4352c54bea9
Szymon Katra <szymon.katra@gmail.com>
2019-11-17 16:37:47 +0000

    RPi basic SPI. TODO: SPI transfer full-duplex

--- examples/rpi_wip/app/main.cpp
+++ examples/rpi_wip/app/main.cpp
@@ -8,24 +8,13 @@ iotlib::Library libGlobal;
 
 int main()
 {
-    //iotlib::UARTPort uart(iotlib::rpi::UART_Primary, 9600, iotlib::UARTPort::Parity::None, iotlib::UARTPort::StopBits::One);
-    //std::cout << "uart created" << std::endl;
-    //uart.print("test");
-    /*iotlib::Gpio gpio(iotlib::rpi::GPIO4, iotlib::Gpio::Direction::Output, false);
-    gpio.write(true);
-    iotlib::System::waitUs(5);
-    gpio.write(false);
-    iotlib::System::waitUs(10);
-    gpio.write(true);
-    iotlib::System::waitUs(15);
-    gpio.write(false);
-    iotlib::System::waitUs(20);
-    gpio.write(true);
-    iotlib::System::waitUs(100);
-    gpio.write(false);*/
-    
-    iotlib::I2CBus i2c(iotlib::rpi::I2C_Bus0, iotlib::rpi::I2C_SDA_GPIO2, iotlib::rpi::I2C_SCL_GPIO3);
-    iotlib::BME280 bme(i2c);
+    iotlib::SPIBus spi(iotlib::rpi::SPI_Bus0);
+    //uint8_t arr[] = { 0xAA, 0xBB, 0xCC };
+    //spi.write(arr, 3);
+
+    //iotlib::I2CBus i2c(iotlib::rpi::I2C_Bus0, iotlib::rpi::I2C_SDA_GPIO2, iotlib::rpi::I2C_SCL_GPIO3);
+    //iotlib::BME280 bme(i2c);
+    iotlib::BME280 bme(spi, iotlib::rpi::GPIO21);
     iotlib::System::sleep(2000);
     iotlib::BME280::Result res;
     bme.getData(res);
