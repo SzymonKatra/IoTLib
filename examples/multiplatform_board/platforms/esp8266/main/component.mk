9bb7517de7456a56d52bf34a0d56b05353837056
Szymon Katra <szymon.katra@gmail.com>
2019-12-02 22:16:39 +0100

    Build scripts for esp8266 and Rpi - multiplatform_board

---
+++ examples/multiplatform_board/platforms/esp8266/main/component.mk
@@ -0,0 +1,3 @@
+COMPONENT_ADD_INCLUDEDIRS := ../../../app ../../..
+COMPONENT_SRCDIRS := ../../../app ../../../iotlib ../../../iotlib/Devices ../../../iotlib/Internal/Platforms/Esp8266 ../../../iotlib/Internal/Platforms/Generic ../../../iotlib/Internal/BoschSensortecBME280_driver
+CPPFLAGS += -DIOTLIB_ESP8266 -DIOTLIB_BOOTSTRAP
